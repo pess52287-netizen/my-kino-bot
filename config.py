9eb9735fb08d2cdcf6a55ac1b366dc899db791cc
pess52287-netizen <pess52287@gmail.com>
2026-07-31 00:52:07 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8817752647:AAHuEY8BdgfEMBsrtBss1ubC6hoM
 
 # Administrator Telegram IDs (List of integers). Only these users can add/delete movies.
 # You can get your ID from bots like @userinfobot or @dtgbot
-ADMIN_IDS = [int(id_str) for id_str in os.environ.get("ADMIN_IDS", "7637932499").split(",") if id_str.strip()]
+ADMIN_IDS = [int(id_str) for id_str in os.environ.get("ADMIN_IDS", "8228654329").split(",") if id_str.strip()]
